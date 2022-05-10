bc7c58588b905b0174c4637778f6825f775cc48c
carlos <carlosdummy14@gmail.com>
2022-05-09 23:30:40 -0500

    work: Employees List, get from api and delete

--- src/api/employee.api.js
+++ src/api/employee.api.js
@@ -1,11 +1,21 @@
 import axios from 'axios'
 
+const BASE_URI = 'http://127.0.0.1:4000/api/v1/employee'
+
 const addEmployee = async (employee) => {
-  const response = await axios.post(
-    'http://127.0.0.1:4000/api/v1/employee',
-    employee
-  )
+  const response = await axios.post(BASE_URI, employee)
   return response
 }
 
-export { addEmployee }
+const getAllEmployees = async () => {
+  const response = await axios.get(BASE_URI)
+  const data = await response.data
+  return data
+}
+
+const deleteEmployee = async (employeeId) => {
+  const response = await axios.delete(`${BASE_URI}/${employeeId}`)
+  return response
+}
+
+export { addEmployee, getAllEmployees, deleteEmployee }
--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,29 +1,39 @@
 import style from './EmployeeList.module.css'
-import bdemployees from '../data/bdemployees'
 import { useEffect, useState } from 'react'
+import { getAllEmployees, deleteEmployee } from '../api/employee.api'
 
 const EmployeeList = () => {
   const [employees, setEmployees] = useState([])
 
+  const getData = async () => {
+    const { data } = await getAllEmployees()
+    setEmployees(data)
+  }
+
   useEffect(() => {
-    setEmployees(bdemployees)
+    getData()
   }, [])
+
+  const handleDelete = async (employeeId) => {
+    await deleteEmployee(employeeId)
+    getData()
+  }
 
   return (
     <div className={style.container}>
       <h3>Employee List</h3>
       <input type='text' name='search' />
       <ul className={style.list}>
-        {employees.map((employee) => {
+        {employees?.map((employee) => {
           return (
-            <li className={style.item} key={employee.id}>
+            <li className={style.item} key={employee._id}>
               <div className={style.image}>
                 <img src={employee.avatar} alt={employee.name} loading='lazy' />
               </div>
               <div className={style.profile}>
                 <span
                   className={style.name}
-                  title={`id: ${employee.individualId}`}
+                  title={`id: ${employee.internalId}`}
                 >
                   {employee.name}
                 </span>
@@ -32,7 +42,7 @@ const EmployeeList = () => {
               </div>
               <div>
                 <button>✏️</button>
-                <button>❌️</button>
+                <button onClick={() => handleDelete(employee._id)}>❌️</button>
               </div>
             </li>
           )
