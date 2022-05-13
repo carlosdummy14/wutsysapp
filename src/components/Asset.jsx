196b09fb103142246c85947e241fae4e7b63e1f7
carlos <carlosdummy14@gmail.com>
2022-05-12 23:37:55 -0500

    feat: add Asset component

---
+++ src/components/Asset.jsx
@@ -0,0 +1,44 @@
+import style from './Asset.module.css'
+
+const ASSET_IMAGE = {
+  HARDWARE: '../images/hardware.png',
+  SOFTWARE: '../images/software.png',
+  OTHER: '../images/other.png',
+}
+
+const Asset = ({
+  assetId,
+  genericName,
+  internalId,
+  brand,
+  model,
+  serialNumber,
+  type,
+  comments,
+  handleDelete,
+}) => {
+  return (
+    <li className={style.item} key={assetId}>
+      <div className={style.image}>
+        <img src={ASSET_IMAGE[type]} alt={genericName} loading='lazy' />
+      </div>
+      <div className={style.profile}>
+        <span className={style.name} title={`id: ${internalId}`}>
+          {internalId}
+          {'  '}
+          {genericName}
+        </span>
+        <span className={style.other}>{brand}</span>
+        <span className={style.other}>{model}</span>
+        <span className={style.other}>{serialNumber}</span>
+        <span className={style.other}>{comments}</span>
+      </div>
+      <div>
+        <button>✏️</button>
+        <button onClick={() => handleDelete(assetId)}>❌️</button>
+      </div>
+    </li>
+  )
+}
+
+export default Asset
