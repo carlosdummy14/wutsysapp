c55a582e27e7e01dc7a45ebfc056eee16af4ec40
Carlos <carlosdummy14@gmail.com>
2022-05-13 16:26:52 -0500

    feat: add Employee component

---
+++ src/components/Employee.jsx
@@ -0,0 +1,24 @@
+import style from './Employee.module.css'
+
+const Employee = ({ employeeId, avatar, name, internalId, position, area, handleDelete }) => {
+  return (
+    <li className={style.item} key={employeeId}>
+      <div className={style.image}>
+        <img src={avatar} alt={name} loading='lazy' />
+      </div>
+      <div className={style.profile}>
+        <span className={style.name} title={`id: ${internalId}`}>
+          {name}
+        </span>
+        <span className={style.other}>{position}</span>
+        <span className={style.other}>{area}</span>
+      </div>
+      <div>
+        <button>✏️</button>
+        <button onClick={() => handleDelete(employeeId)}>❌️</button>
+      </div>
+    </li>
+  )
+}
+
+export default Employee
