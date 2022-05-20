37c60dd1fb5fe8a6a26795d2e2dd53957e509d6e
Carlos <carlosdummy14@gmail.com>
2022-05-20 17:15:46 -0500

    work: style for Employee Selected panel on AssignAsset

--- src/components/AssignAsset.jsx
+++ src/components/AssignAsset.jsx
@@ -1,45 +1,40 @@
 import style from './AssignAsset.module.css'
+import EmployeeList from './EmployeeList'
 
 const AssignAsset = () => {
   return (
     <div className={style.container}>
       <div style={{ backgroundColor: 'blue' }}>
         <h4>Employees Availables</h4>
-        <ul>
-          <li>item 1</li>
-          <li>item 2</li>
-          <li>item 3</li>
-          <li>item 4</li>
-          <li>item 5</li>
-        </ul>
+        <EmployeeList />
       </div>
 
       <div className={style['employee-selected']}>
-        <h4>Employee Selected</h4>
-        <div className={style.profile}>
-          <div className={style.img}>
+        <h4 className={style.title}>Employee Selected</h4>
+        <div className={style.card}>
+          <div className={style.image}>
             <img src='' alt='' />
           </div>
-          <div className={style.text}>
-            <span>Name</span>
-            <span>Position</span>
-            <span>Area</span>
+          <div className={style.profile}>
+            <span className={style.name}>Name</span>
+            <span className={style.other}>Position</span>
+            <span className={style.other}>Area</span>
           </div>
         </div>
         <div className={style.resume}>
-          <h4>Resume</h4>
+          <h4 className={style.title}>Resume</h4>
           <div className={style['resume-data']}>
-            <span>Hardware</span>
-            <span>10</span>
-            <span>Software</span>
-            <span>100</span>
-            <span>Other</span>
-            <span>100</span>
+            <span className={style.label}>Hardware</span>
+            <span className={style.amount}>10</span>
+            <span className={style.label}>Software</span>
+            <span className={style.amount}>100</span>
+            <span className={style.label}>Other</span>
+            <span className={style.amount}>100</span>
           </div>
         </div>
         <div className={style.buttons}>
-          <button>Apply</button>
-          <button>Cancel</button>
+          <button className={style.button}>Apply</button>
+          <button className={style.button}>Cancel</button>
         </div>
       </div>
 
